36fe409012410355232f26ff779e017d28f7fb11
Nadya Kadayifci <nadkad@Nadyas-MBP.home>
2019-05-08 23:17:02 -0400

    testing mobile again

--- banner-slideshow.js
+++ banner-slideshow.js
@@ -1,8 +1,8 @@
-// $(document).ready(function(){
-//  $('.banner-images').slick({
-//    autoplay: true,
-//    autoplaySpeed: 3000,
-//    arrows: false,
-//    fade: true
-//  });
-//});
+ $(document).ready(function(){
+  $('.banner-images').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    fade: true
+  });
+});
